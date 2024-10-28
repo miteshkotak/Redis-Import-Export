c93afe63ea65c5521b768ccb1f31ea9e554f676c
Mitesh <miteshkotak7@gmail.com>
2024-10-28 14:37:51 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+Explanation of Changes:
+
+Class Definition: Created a RedisExporter class to encapsulate the functionality.
+Constructor: Added a constructor to initialize the source and destination Redis connections.
+
+Methods: Split the functionality into separate methods (exportData, importData, and closeConnections) for better organization.
+
+Usage: Updated the usage section to create an instance of RedisExporter and call the method to export and import the collection.
+
+This structure improves readability and maintainability of the code.
---
+++ src/index.ts
@@ -0,0 +1,95 @@
+import Redis from 'ioredis'
+
+interface ExportData {
+    [key: string]: any 
+}
+
+
+class RedisExporter {
+    sourceRedis: any
+    destRedis: any
+    constructor(sourceConfig: any, destConfig: any) {
+        this.sourceRedis = new Redis(sourceConfig)
+        this.destRedis  = new Redis(destConfig)
+    }
+
+    async exportAndImportCollection(collectionName: String) {
+        try {
+            const exportData = await this.exportData(collectionName)
+            console.log(`Exported ${Object.keys(exportData).length} keys`)
+            await this.importData(exportData)
+            console.log(`Imported ${Object.keys(exportData).length} keys`)
+        } catch (error) {
+            console.error('Error during export/import:', error)
+        } finally {
+            await this.closeConnections()
+        }
+    }
+
+    async exportData(collectionName: String) {
+        const keys = await this.sourceRedis.keys(collectionName)
+        const exportData: ExportData = {}
+
+        for (const key of keys) {
+            const type = await this.sourceRedis.type(key)
+            switch (type) {
+                case 'string':
+                    exportData[key] = await this.sourceRedis.get(key)
+                    break
+                case 'list':
+                    exportData[key] = await this.sourceRedis.lrange(key, 0, -1)
+                    break
+                case 'hash':
+                    exportData[key] = await this.sourceRedis.hgetall(key)
+                    break
+                case 'set':
+                    exportData[key] = await this.sourceRedis.smembers(key)
+                    break
+                case 'zset':
+                    exportData[key] = await this.sourceRedis.zrange(key, 0, -1, 'WITHSCORES')
+                    break
+                default:
+                    console.warn(`Skipping key ${key} of type ${type}`)
+            }
+        }
+        return exportData;
+    }
+
+    async importData(exportData: ExportData) {
+        for (const [key, value] of Object.entries(exportData)) {
+            const type = await this.sourceRedis.type(key)
+            switch (type) {
+                case 'string':
+                    await this.destRedis.set(key, value)
+                    break
+                case 'list':
+                    await this.destRedis.rpush(key, ...(value as string[]))
+                    break
+                case 'hash':
+                    await this.destRedis.hmset(key, value)
+                    break
+                case 'set':
+                    await this.destRedis.sadd(key, ...(value as string[]))
+                    break
+                case 'zset':
+                    await this.destRedis.zadd(key, ...(value as string[]))
+                    break
+                default:
+                    console.warn(`Skipping import for key ${key} of type ${type}`)
+            }
+        }
+    }
+
+    async closeConnections() {
+        await this.sourceRedis.quit()
+        await this.destRedis.quit()
+    }
+}
+
+// Usage
+const sourceConfig = { host: '127.0.0.1', port: 6380 }
+const destConfig = { host: '127.0.0.1', port: 6379 }
+const collectionName = process.argv[2]
+
+const redisExporter = new RedisExporter(sourceConfig, destConfig)
+redisExporter.exportAndImportCollection(collectionName)
