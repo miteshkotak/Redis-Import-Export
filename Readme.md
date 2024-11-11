4de1f941006c882b2a8c8b207e4213f011a6eeac
Mitesh <miteshkotak7@gmail.com>
2024-11-11 17:09:53 +0100

    update export and import function to work independently too

--- Readme.md
+++ Readme.md
@@ -1,3 +1,10 @@
 
 
 Usage: Updated the usage section to create an instance of RedisExporter and call the method to export and import the collection.
+
+List of Methods
+
+- listCollections
+- exportAndImportCollection
+- exportData
+- importData 
--- build/index.js
+++ build/index.js
@@ -24,6 +24,9 @@ class RedisExporter {
     async exportAndImportCollection(collectionName) {
         try {
             const exportData = await this.exportData(collectionName);
+            if (!exportData) {
+                throw new Error('Export failed - no data returned');
+            }
             console.log(`Exported ${Object.keys(exportData).length} keys`);
             await this.importData(exportData);
             console.log(`Imported ${Object.keys(exportData).length} keys`);
@@ -38,52 +41,68 @@ class RedisExporter {
     async exportData(collectionName) {
         const keys = await this.sourceRedis.keys(collectionName);
         const exportData = {};
-        for (const key of keys) {
-            const type = await this.sourceRedis.type(key);
-            switch (type) {
-                case 'string':
-                    exportData[key] = await this.sourceRedis.get(key);
-                    break;
-                case 'list':
-                    exportData[key] = await this.sourceRedis.lrange(key, 0, -1);
-                    break;
-                case 'hash':
-                    exportData[key] = await this.sourceRedis.hgetall(key);
-                    break;
-                case 'set':
-                    exportData[key] = await this.sourceRedis.smembers(key);
-                    break;
-                case 'zset':
-                    exportData[key] = await this.sourceRedis.zrange(key, 0, -1, 'WITHSCORES');
-                    break;
-                default:
-                    console.warn(`Skipping key ${key} of type ${type}`);
+        try {
+            for (const key of keys) {
+                const type = await this.sourceRedis.type(key);
+                switch (type) {
+                    case 'string':
+                        exportData[key] = await this.sourceRedis.get(key);
+                        break;
+                    case 'list':
+                        exportData[key] = await this.sourceRedis.lrange(key, 0, -1);
+                        break;
+                    case 'hash':
+                        exportData[key] = await this.sourceRedis.hgetall(key);
+                        break;
+                    case 'set':
+                        exportData[key] = await this.sourceRedis.smembers(key);
+                        break;
+                    case 'zset':
+                        exportData[key] = await this.sourceRedis.zrange(key, 0, -1, 'WITHSCORES');
+                        break;
+                    default:
+                        console.warn(`Skipping key ${key} of type ${type}`);
+                }
+            }
+            return exportData;
+        }
+        catch (error) {
+            return console.error('Error during Exporting collection data:', error);
+        }
+        finally {
+            await this.closeConnections();
+        }
+    }
+    async importData(importData) {
+        try {
+            for (const [key, value] of Object.entries(importData)) {
+                const type = await this.sourceRedis.type(key);
+                switch (type) {
+                    case 'string':
+                        await this.destRedis.set(key, value);
+                        break;
+                    case 'list':
+                        await this.destRedis.rpush(key, ...value);
+                        break;
+                    case 'hash':
+                        await this.destRedis.hmset(key, value);
+                        break;
+                    case 'set':
+                        await this.destRedis.sadd(key, ...value);
+                        break;
+                    case 'zset':
+                        await this.destRedis.zadd(key, ...value);
+                        break;
+                    default:
+                        await this.destRedis.set(key, JSON.stringify(value));
+                }
             }
         }
-        return exportData;
-    }
-    async importData(exportData) {
-        for (const [key, value] of Object.entries(exportData)) {
-            const type = await this.sourceRedis.type(key);
-            switch (type) {
-                case 'string':
-                    await this.destRedis.set(key, value);
-                    break;
-                case 'list':
-                    await this.destRedis.rpush(key, ...value);
-                    break;
-                case 'hash':
-                    await this.destRedis.hmset(key, value);
-                    break;
-                case 'set':
-                    await this.destRedis.sadd(key, ...value);
-                    break;
-                case 'zset':
-                    await this.destRedis.zadd(key, ...value);
-                    break;
-                default:
-                    console.warn(`Skipping import for key ${key} of type ${type}`);
-            }
+        catch (error) {
+            return console.error('Error during Importing Data:', error);
+        }
+        finally {
+            await this.closeConnections();
         }
     }
     async closeConnections() {
@@ -93,8 +112,19 @@ class RedisExporter {
 }
 exports.RedisExporter = RedisExporter;
 // const collectionName = 'sample_jobQueue:*' // Get collection name from request body
-// const sourceConfig = { host: 'localhost', port: 6380 }
+const sourceConfig = { host: 'localhost', port: 6380 };
 // const destConfig = { host: 'localhost', port: 6379 }
-// const redisExporter = new RedisExporter(sourceConfig, destConfig)
+const redisExporter = new RedisExporter(sourceConfig);
 // redisExporter.listCollections()
 // redisExporter.exportAndImportCollection(collectionName)
+// const importData = JSON.parse(fs.readFileSync('redis_data.json', 'utf-8')); // Read data from JSON file // Get collection name from request body
+async function mainv1(collectionName) {
+    try {
+        const data = await redisExporter.exportData(collectionName);
+        console.log('success:', data);
+    }
+    catch (error) {
+        console.log('error');
+    }
+}
+mainv1('sample_session:*');
