45eef719f0ce80a286a54e5ce5d1b1d51abaa38d
Mitesh <miteshkotak7@gmail.com>
2024-12-07 09:15:32 +0100

    export redis collection to dumb.rdb file

--- build/index.js
+++ build/index.js
@@ -64,6 +64,7 @@ class RedisIO {
                         console.warn(`Skipping key ${key} of type ${type}`);
                 }
             }
+            await this.saveToFile('dump.rdb', exportData);
             return exportData;
         }
         catch (error) {
@@ -72,6 +73,11 @@ class RedisIO {
         finally {
             await this.closeConnections();
         }
+    }
+    // New method to save data to a file
+    async saveToFile(filename, data) {
+        const fs = require('fs').promises;
+        await fs.writeFile(filename, JSON.stringify(data));
     }
     async importData(importData, expiryDays) {
         try {
@@ -117,21 +123,22 @@ class RedisIO {
 exports.RedisIO = RedisIO;
 //Example on how to use
 // const collectionName = 'sample_jobQueue:*' // Get collection name from request body
-// const sourceConfig = { host: 'localhost', port: 6380 }
+const sourceConfig = { host: 'localhost', port: 6379 };
 // // // const destConfig = { host: 'localhost', port: 6379 }
-// const redisExporter = new RedisIO(sourceConfig)
-// // // redisExporter.listCollections()
+const redisExporter = new RedisIO(sourceConfig);
+redisExporter.listCollections();
 // // // redisExporter.exportAndImportCollection(collectionName)
 // // // const importData = JSON.parse(fs.readFileSync('redis_data.json', 'utf-8')); // Read data from JSON file // Get collection name from request body
-// let exportData : any
-// async function mainExport (collectionName: any) {
-//     try {
-//         exportData = await redisExporter.exportData(collectionName)
-//         console.log('success:', exportData)
-//     } catch (error) {
-//         console.log('error')
-//     }
-// }
+let exportData;
+async function mainExport(collectionName) {
+    try {
+        exportData = await redisExporter.exportData(collectionName);
+        console.log('success:', exportData);
+    }
+    catch (error) {
+        console.log('error');
+    }
+}
 // // async function mainImport (importData: any) {
 // //     try {
 // //         const data = await redisExporter.importData(importData)
@@ -140,5 +147,5 @@ exports.RedisIO = RedisIO;
 // //         console.log('error')
 // //     }
 // // }
-// //mainExport('sample_jobQueue:*')
+mainExport('sample_jobQueue:*');
 // mainImport(importData)
